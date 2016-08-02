1ddc9bcb2981dc3eae26bf9c6d77d4c863b3ab6e
shuai.li <286287737@qq.com>
2016-08-02 22:13:23 +0800

    transform

--- src/raw_transformer.py
+++ src/raw_transformer.py
@@ -5,18 +5,30 @@
 from common import *
 
 class RawTransformer:
-    def __init__(self, normal, ex):
+    def __init__(self, normal, ex=None):
         self.normal = pd.read_pickle(normal)
-        self.ex = pd.read_pickle(ex)
+        # self.ex = pd.read_pickle(ex)
     
     def ParseNormal(self):
         self.normal = self.normal.set_index("date")
-        # v.columns = ["open", "high", "low", "close", "vol", "tun", "id"]
-        # v.set_index(["id"], append=True)
+        ans = None
+        # for i in range(0, len(self.normal.columns), 6):
+        for i in range(0, 12, 6):
+            stock = self.normal.ix[:, i:i + 6].copy()
+            stock["id"] = stock.columns[0]
+            stock.columns = ["open", "high", "low", "close", "vol", "tun", "id"]
+            stock = stock.set_index(["id"], append=True)
+            if ans is None:
+                ans = stock
+            else:
+                ans = ans.add(stock, fill_value=0)
+        return ans[:4]
         # df.index = pd.MultiIndex.from_tuples(index, names = names)
         # df.index.set_levels([u'Total', u'x2'],level=1,inplace=True)
         # df.index.set_labels([0, 1],level=1,inplace=True)
         # s.add(v, fill_value=0)
 
 if __name__ == '__main__':
-    print np.arange(3)
+    rt = RawTransformer("../data/sh14_normal.pkl")
+    s = rt.ParseNormal()
+    print s
