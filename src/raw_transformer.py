da13170652d076cbef646d194685547971ab5c8f
shuai.li <286287737@qq.com>
2016-08-03 10:48:32 +0800

    transform

--- src/raw_transformer.py
+++ src/raw_transformer.py
@@ -5,16 +5,14 @@
 from common import *
 
 class RawTransformer:
-    def __init__(self, normal, ex=None):
-        self.normal = pd.read_pickle(normal)
-        # self.ex = pd.read_pickle(ex)
-    
-    def ParseNormal(self):
-        self.normal = self.normal.set_index("date")
+    @staticmethod
+    def parseNormal(normal_file):
+        normal = pd.read_pickle(normal_file)
+        normal = normal.set_index("date")
         ans = None
-        # for i in range(0, len(self.normal.columns), 6):
-        for i in range(0, 12, 6):
-            stock = self.normal.ix[:, i:i + 6].copy()
+        bound = len(normal.columns)
+        for i in range(0, bound, 6):
+            stock = normal.ix[:, i:i + 6].copy()
             stock["id"] = stock.columns[0]
             stock.columns = ["open", "high", "low", "close", "vol", "tun", "id"]
             stock = stock.set_index(["id"], append=True)
@@ -22,13 +20,38 @@ class RawTransformer:
                 ans = stock
             else:
                 ans = ans.add(stock, fill_value=0)
-        return ans[:4]
+        return ans
+    
+    @staticmethod
+    def parseEx(ex_file):
+        ex = pd.read_pickle(ex_file)
+        bound = len(ex.columns)
+        print "trim", bound % 7
+        bound = bound / 7 * 7
+        ans = None
+        print bound
+        for i in range(0, bound, 7):
+            print i
+            stock = ex.ix[:, i:i + 5].copy()
+            stock["id"] = stock.columns[0]
+            stock.columns = ["ex0", "ex1", "ex2", "ex3", "ex4", "id"]
+            stock = stock.set_index(["id"], append=True)
+            if ans is None:
+                ans = stock
+            else:
+                ans = ans.add(stock, fill_value=0)
+        return ans
+        
         # df.index = pd.MultiIndex.from_tuples(index, names = names)
         # df.index.set_levels([u'Total', u'x2'],level=1,inplace=True)
         # df.index.set_labels([0, 1],level=1,inplace=True)
         # s.add(v, fill_value=0)
 
 if __name__ == '__main__':
-    rt = RawTransformer("../data/sh14_normal.pkl")
-    s = rt.ParseNormal()
-    print s
+    rt = RawTransformer()
+    # s = rt.parseNormal("../data/sh14_normal.pkl")
+    # s.to_pickle("../data/
+    print time.ctime()
+    s = rt.parseEx("../data/sh14_ex.pkl")
+    s.to_pickle("../data/sh14_ex_multindex.pkl")
+    print time.ctime()
