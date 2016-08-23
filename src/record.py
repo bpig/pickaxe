e756ef946f812b653f374a321bc8cdec704e2835
shuai.li <286287737@qq.com>
2016-08-23 16:39:14 +0800

    transform

--- src/record.py
+++ src/record.py
@@ -15,9 +15,10 @@ class Record:
         
         self.ans = []
         keys = set(df.index.get_level_values('id'))
-        for key in keys:
+        for c, key in enumerate(keys):
             self.parse_st(key)
-            return
+            if c % 10 == 0:
+                print time.ctime(), c
     
     def get_daily_cap(self):
         self.cap = {}
@@ -38,6 +39,12 @@ class Record:
         p[p == 5] = 4
         return p
     
+    def dump(self):
+        fout = open("test.dump", "w")
+        for r in self.ans:
+            r = [str(_) for _ in r]
+            fout.write(",".join(r) + "\n")
+    
     @staticmethod
     def get_stats(ary):
         ary = (ary - ary.mean()) / ary.std()
@@ -53,7 +60,6 @@ class Record:
         st['target'] = self.parse_target(st['close'].get_values())
         del st['close'], st['vol']
         step = 10
-        print len(st)
         for i in range(0, len(st) - step):
             swing = np.array([st.iloc[i + _, 0] for _ in range(step)])
             swing_ex = self.get_stats(swing)
@@ -67,7 +73,4 @@ if __name__ == '__main__':
     os.chdir("../data")
     rd = Record()
     print len(rd.ans)
-    print rd.ans[0]
-    print rd.ans[10]
-    print rd.ans[100]
-    print rd.ans[400]
+    rd.dump()
