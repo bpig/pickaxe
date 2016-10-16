0be9f60d3b8a029e5e9dfdfc4dafee8fc231245b
austin <286287737@qq.com>
2016-10-16 11:04:13 +0800

    format......

---
+++ src/test/test_fea.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+from unittest import TestCase
+
+__author__ = "shuai.li(286287737@qq.com)"
+__date__ = "10/16/16"
+
+class TestFea(TestCase):
+    def test_main(self):
+        self.fail()
