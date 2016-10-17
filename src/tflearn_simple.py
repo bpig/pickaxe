9e4bad68f2b7cb2f639e6ceb34cbdbecb11034bf
lee <286287737@qq.com>
2016-10-17 11:42:18 +0800

    update........

--- src/tflearn_simple.py
+++ src/tflearn_simple.py
@@ -1,16 +1,20 @@
 from sklearn import datasets
 from sklearn import metrics
 import numpy as np
+import random
+import time
 import tensorflow as tf
 import tensorflow.contrib.layers.python.layers as layers
 import tensorflow.contrib.learn.python.learn as learn
 
 def tgtMap(tgt):
-    return 0 if tgt < 1.102 else 1
+    return 0 if tgt < 1.002 else 1
 
 def loadFea(filename):
     datas = []
     tgts = []
+    a = []
+    ct = [0] * 2
     for l in open(filename):
         l = l.strip()
         if not l:
@@ -20,15 +24,27 @@ def loadFea(filename):
         value = l[pos + 1:].split(",")
         tgt = tgtMap(float(value[-1]))
         value = np.asarray(value[:-1]).astype(np.float32)
+        a += [(value, tgt)]
+        ct[tgt] += 1
 
-        datas += [value]
-        tgts += [tgt]
+    print ct
+    random.shuffle(a)
+    #a = a[:100000]
+    point = int(len(a) * 0.9)
+    train = a[:point]
+    test = a[point:]
+    
+    train_data, train_tgt = zip(*train)
+    test_data, test_tgt = zip(*test)
+    # datas = np.array(datas)
+    # tgts = np.array(tgts)
+    return np.array(train_data), np.array(train_tgt), np.array(test_data), np.array(test_tgt)
 
-    datas = np.array(datas)
-    tgts = np.array(tgts)
-    return datas, tgts
-
-data, target = loadFea("../data/fe_20150907.cmvn")
+#data, target = loadFea("../data/fe_20150907.cmvn")
+#data, target, test_data, test_tgt = loadFea("../data/fe_20150907.cmvn")
+print time.ctime()
+data, target, test_data, test_tgt = loadFea("../data/2015.fe.cmvn")
+print time.ctime()
 
 def my_model(features, target):
     """DNN with three hidden layers, and dropout of 0.1 probability."""
@@ -38,7 +54,7 @@ def my_model(features, target):
 
     # Create three fully connected layers respectively of size 10, 20, and 10 with
     # each layer having a dropout probability of 0.1.
-    features = layers.stack(features, layers.fully_connected, [1000, 200, 20])
+    features = layers.stack(features, layers.fully_connected, [1000, 400, 400, 100, 10])
 
     # Create two tensors respectively for prediction and loss.
     prediction, loss = (
@@ -47,20 +63,20 @@ def my_model(features, target):
 
     # Create a tensor for training op.
     train_op = tf.contrib.layers.optimize_loss(
-        loss, tf.contrib.framework.get_global_step(), optimizer='Adagrad',
-        learning_rate=0.1)
+        loss, tf.contrib.framework.get_global_step(), optimizer='Adam',
+        learning_rate=0.001)
 
     return {'class': tf.argmax(prediction, 1), 'prob': prediction}, loss, train_op
 
-classifier = learn.Estimator(model_fn=my_model)
+classifier = learn.Estimator(model_fn=my_model, model_dir="m0.001_dl")
 print data.shape
 #print data[0]
 print target.shape
 print target[0]
 
 classifier.fit(data, target, steps=2000)
-
+print time.ctime()
 y_predicted = [
-  p['class'] for p in classifier.predict(data, as_iterable=True)]
-score = metrics.accuracy_score(target, y_predicted)
+  p['class'] for p in classifier.predict(test_data, as_iterable=True)]
+score = metrics.accuracy_score(test_tgt, y_predicted)
 print('Accuracy: {0:f}'.format(score))
