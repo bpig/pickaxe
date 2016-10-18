1144d067e42cf84581a5b9b2b3e6aafd407e2e2b
lee <286287737@qq.com>
2016-10-18 14:41:06 +0800

    update......

--- src/tflearn_simple.py
+++ src/tflearn_simple.py
@@ -2,9 +2,12 @@ from sklearn import metrics
 import numpy as np
 import random
 import time
+import sys
 import tensorflow as tf
 import tensorflow.contrib.layers.python.layers as layers
 import tensorflow.contrib.learn.python.learn as learn
+
+tf.logging.set_verbosity(tf.logging.INFO)
 
 def tgtMap(tgt):
     return 0 if tgt < 1.002 else 1
@@ -40,35 +43,34 @@ def loadFea(filename):
     return np.array(train_data), np.array(train_tgt), np.array(test_data), np.array(test_tgt)
 
 #data, target = loadFea("../data/fe_20150907.cmvn")
-#data, target, test_data, test_tgt = loadFea("../data/fe_20150907.cmvn")
+
 print time.ctime()
-data, target, test_data, test_tgt = loadFea("../data/2015.fe.cmvn")
+#data, target, test_data, test_tgt = loadFea("data/fe_20150907.cmvn")
+data, target, test_data, test_tgt = loadFea("data/2015.fe")
 print time.ctime()
 
 def my_model(features, target):
     """DNN with three hidden layers, and dropout of 0.1 probability."""
     # Convert the target to a one-hot tensor of shape (length of features, 3) and
     # with a on-value of 1 for each one-hot vector of length 3.
-    target = tf.one_hot(target, 2, 1, 0)
     
-    # Create three fully connected layers respectively of size 10, 20, and 10 with
-    # each layer having a dropout probability of 0.1.
+    # target = tf.one_hot(target, 2, 1, 0)
+    
+    features = layers.stack(features, layers.fully_connected, [800, 100, 10])
 
-    features = layers.stack(features, layers.fully_connected, [1000, 400, 400, 100, 10])
-
-    # Create two tensors respectively for prediction and loss.
     prediction, loss = (
         tf.contrib.learn.models.logistic_regression(features, target)
     )
     
-    # Create a tensor for training op.
     train_op = tf.contrib.layers.optimize_loss(
-        loss, tf.contrib.framework.get_global_step(), optimizer='Adam',
-        learning_rate=0.001)
+            loss, tf.contrib.framework.get_global_step(), optimizer='Adam',
+            learning_rate=0.001)
 
     return {'class': tf.argmax(prediction, 1), 'prob': prediction}, loss, train_op
 
-classifier = learn.Estimator(model_fn=my_model, model_dir="m0.001_dl")
+# with tf.device('/gpu:0'):
+classifier = learn.Estimator(model_fn=my_model, model_dir="model/" + sys.argv[1])
+
 print data.shape
 print target.shape
 print target[0]
