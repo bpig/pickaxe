a37da482beebc34ab76a7d67e001ce48dbbd5f88
lee <286287737@qq.com>
2016-10-19 17:05:52 +0800

    update.........

--- src/mlp_feeder.py
+++ src/mlp_feeder.py
@@ -24,130 +24,30 @@ from __future__ import division
 from __future__ import print_function
 
 import gzip
-
-import numpy
+import os
+import collections
+import numpy as np
 from six.moves import xrange  # pylint: disable=redefined-builtin
 
-from tensorflow.contrib.learn.python.learn.datasets import base
+Datasets = collections.namedtuple('Datasets', ['train','test'])
+
 from tensorflow.python.framework import dtypes
 
-SOURCE_URL = 'http://yann.lecun.com/exdb/mnist/'
-
-def _read32(bytestream):
-    dt = numpy.dtype(numpy.uint32).newbyteorder('>')
-    return numpy.frombuffer(bytestream.read(4), dtype=dt)[0]
-
-def extract_images(f):
-    """Extract the images into a 4D uint8 numpy array [index, y, x, depth].
-  
-    Args:
-      f: A file object that can be passed into a gzip reader.
-  
-    Returns:
-      data: A 4D unit8 numpy array [index, y, x, depth].
-  
-    Raises:
-      ValueError: If the bytestream does not start with 2051.
-  
-    """
-    print('Extracting', f.name)
-    with gzip.GzipFile(fileobj=f) as bytestream:
-        magic = _read32(bytestream)
-        if magic != 2051:
-            raise ValueError('Invalid magic number %d in MNIST image file: %s' %
-                             (magic, f.name))
-        num_images = _read32(bytestream)
-        rows = _read32(bytestream)
-        cols = _read32(bytestream)
-        buf = bytestream.read(rows * cols * num_images)
-        data = numpy.frombuffer(buf, dtype=numpy.uint8)
-        data = data.reshape(num_images, rows, cols, 1)
-        return data
-
-def dense_to_one_hot(labels_dense, num_classes):
-    """Convert class labels from scalars to one-hot vectors."""
-    num_labels = labels_dense.shape[0]
-    index_offset = numpy.arange(num_labels) * num_classes
-    labels_one_hot = numpy.zeros((num_labels, num_classes))
-    labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
-    return labels_one_hot
-
-def extract_labels(f, one_hot=False, num_classes=10):
-    """Extract the labels into a 1D uint8 numpy array [index].
-  
-    Args:
-      f: A file object that can be passed into a gzip reader.
-      one_hot: Does one hot encoding for the result.
-      num_classes: Number of classes for the one hot encoding.
-  
-    Returns:
-      labels: a 1D unit8 numpy array.
-  
-    Raises:
-      ValueError: If the bystream doesn't start with 2049.
-    """
-    print('Extracting', f.name)
-    with gzip.GzipFile(fileobj=f) as bytestream:
-        magic = _read32(bytestream)
-        if magic != 2049:
-            raise ValueError('Invalid magic number %d in MNIST label file: %s' %
-                             (magic, f.name))
-        num_items = _read32(bytestream)
-        buf = bytestream.read(num_items)
-        labels = numpy.frombuffer(buf, dtype=numpy.uint8)
-        
-        labels = labels.astype(float) / 10.0
-        # if one_hot:
-        #    return dense_to_one_hot(labels, num_classes)
-        return labels
-
 class DataSet(object):
-    def __init__(self,
-                 images,
-                 labels,
-                 fake_data=False,
-                 one_hot=False,
-                 dtype=dtypes.float32,
-                 reshape=True):
-        """Construct a DataSet.
-        one_hot arg is used only if fake_data is true.  `dtype` can be either
-        `uint8` to leave the input as `[0, 255]`, or `float32` to rescale into
-        `[0, 1]`.
-        """
-        dtype = dtypes.as_dtype(dtype).base_dtype
-        if dtype not in (dtypes.uint8, dtypes.float32):
-            raise TypeError('Invalid image dtype %r, expected uint8 or float32' %
-                            dtype)
-        if fake_data:
-            self._num_examples = 10000
-            self.one_hot = one_hot
-        else:
-            assert images.shape[0] == labels.shape[0], (
-                'images.shape: %s labels.shape: %s' % (images.shape, labels.shape))
-            self._num_examples = images.shape[0]
-            
-            # Convert shape from [num examples, rows, columns, depth]
-            # to [num examples, rows*columns] (assuming depth == 1)
-            if reshape:
-                assert images.shape[3] == 1
-                images = images.reshape(images.shape[0],
-                                        images.shape[1] * images.shape[2])
-            if dtype == dtypes.float32:
-                # Convert from [0, 255] -> [0.0, 1.0].
-                images = images.astype(numpy.float32)
-                images = numpy.multiply(images, 1.0 / 255.0)
-        self._images = images
-        self._labels = labels
+    def __init__(self, feas, tgts):
+        self._feas = feas
+        self._tgts = tgts
         self._epochs_completed = 0
         self._index_in_epoch = 0
+        self._num_examples = len(feas)
     
     @property
-    def images(self):
-        return self._images
+    def feas(self):
+        return self._feas
     
     @property
-    def labels(self):
-        return self._labels
+    def tgts(self):
+        return self._tgts
     
     @property
     def num_examples(self):
@@ -157,92 +57,91 @@ class DataSet(object):
     def epochs_completed(self):
         return self._epochs_completed
     
-    def next_batch(self, batch_size, fake_data=False):
-        """Return the next `batch_size` examples from this data set."""
-        if fake_data:
-            fake_image = [1] * 784
-            if self.one_hot:
-                fake_label = [1] + [0] * 9
-            else:
-                fake_label = 0
-            return [fake_image for _ in xrange(batch_size)], [
-                fake_label for _ in xrange(batch_size)
-                ]
+    def next_batch(self, batch_size):
         start = self._index_in_epoch
         self._index_in_epoch += batch_size
         if self._index_in_epoch > self._num_examples:
             # Finished epoch
             self._epochs_completed += 1
             # Shuffle the data
-            perm = numpy.arange(self._num_examples)
-            numpy.random.shuffle(perm)
-            self._images = self._images[perm]
-            self._labels = self._labels[perm]
+            perm = np.arange(self._num_examples)
+            np.random.shuffle(perm)
+            self._feas = self._feas[perm]
+            self._tgts = self._tgts[perm]
             # Start next epoch
             start = 0
             self._index_in_epoch = batch_size
             assert batch_size <= self._num_examples
         end = self._index_in_epoch
-        return self._images[start:end], self._labels[start:end]
+        return self._feas[start:end], self._tgts[start:end]
 
-def read_data_sets(train_dir,
-                   fake_data=False,
-                   one_hot=False,
-                   dtype=dtypes.float32,
-                   reshape=True,
-                   validation_size=5000):
-    if fake_data:
-        def fake():
-            return DataSet([], [], fake_data=True, one_hot=one_hot, dtype=dtype)
-        
-        train = fake()
-        validation = fake()
-        test = fake()
-        return base.Datasets(train=train, validation=validation, test=test)
-    
-    TRAIN_IMAGES = 'train-images-idx3-ubyte.gz'
-    TRAIN_LABELS = 'train-labels-idx1-ubyte.gz'
-    TEST_IMAGES = 't10k-images-idx3-ubyte.gz'
-    TEST_LABELS = 't10k-labels-idx1-ubyte.gz'
-    
-    local_file = base.maybe_download(TRAIN_IMAGES, train_dir,
-                                     SOURCE_URL + TRAIN_IMAGES)
-    with open(local_file, 'rb') as f:
-        train_images = extract_images(f)
-    
-    local_file = base.maybe_download(TRAIN_LABELS, train_dir,
-                                     SOURCE_URL + TRAIN_LABELS)
-    with open(local_file, 'rb') as f:
-        train_labels = extract_labels(f, one_hot=one_hot)
-    
-    local_file = base.maybe_download(TEST_IMAGES, train_dir,
-                                     SOURCE_URL + TEST_IMAGES)
-    with open(local_file, 'rb') as f:
-        test_images = extract_images(f)
-    
-    local_file = base.maybe_download(TEST_LABELS, train_dir,
-                                     SOURCE_URL + TEST_LABELS)
-    with open(local_file, 'rb') as f:
-        test_labels = extract_labels(f, one_hot=one_hot)
-    
-    if not 0 <= validation_size <= len(train_images):
-        raise ValueError(
-            'Validation size should be between 0 and {}. Received: {}.'
-                .format(len(train_images), validation_size))
-    
-    validation_images = train_images[:validation_size]
-    validation_labels = train_labels[:validation_size]
-    train_images = train_images[validation_size:]
-    train_labels = train_labels[validation_size:]
-    
-    train = DataSet(train_images, train_labels, dtype=dtype, reshape=reshape)
-    validation = DataSet(validation_images,
-                         validation_labels,
-                         dtype=dtype,
-                         reshape=reshape)
-    test = DataSet(test_images, test_labels, dtype=dtype, reshape=reshape)
-    
-    return base.Datasets(train=train, validation=validation, test=test)
 
-def load_mnist(train_dir='MNIST-data'):
-    return read_data_sets(train_dir)
+def tgtMap(tgt):
+    # m1 = 0.7363636363636363
+    # m2 = 1.3444444444444443
+    m1 = 0.8
+    m2 = 1.2
+    if tgt < m1:
+        tgt = 0.8
+    elif tgt > m2:
+        tgt = m2
+    ans = (tgt - m1) / (m2 - m1)
+    # assert ans >= 0.0, ans
+    # assert ans <= 1.0, ans
+    return ans
+
+def loadFea(filename, cv=None):
+    keys = []
+    feas = []
+    tgts = []
+
+    for c, l in enumerate(open(filename)):
+        l = l.strip()
+        if not l:
+            continue
+        pos = l.find(":")
+        key = l[:pos]
+        k, ds = key.split("_")
+        # if ds >= "20160800":
+        #     continue
+        value = l[pos + 1:].split(",")
+        tgt = tgtMap(float(value[-1]))
+        fea = np.asarray(value[:-1]).astype(np.float32)
+
+        keys += [key]
+        feas += [fea]
+        tgts += [tgt]
+        if cv and c == cv:
+            break
+    ct = len(tgts)
+    return keys, np.asarray(feas), np.asarray(tgts).reshape([ct, 1])
+    
+
+def read_data_sets(datafile, cv=None, skip=False):
+    keyfile = datafile + ".key.npy"
+    feafile = datafile + ".fea.npy"
+    tgtfile = datafile + ".tgt.npy"
+
+    if not skip and os.path.exists(keyfile):
+        keys = np.load(keyfile)
+        feas = np.load(feafile)
+        tgts = np.load(tgtfile)
+    else:
+        keys, feas, tgts = loadFea(datafile, cv)
+        np.save(keyfile, keys)
+        np.save(feafile, feas)
+        np.save(tgtfile, tgts)
+
+    point = int(len(keys) * .9)
+
+    tr_x = feas[:point]
+    tr_y = tgts[:point]
+
+    te_x = feas[point:]
+    te_y = tgts[point:]
+    
+    train = DataSet(tr_x, tr_y)
+    test = DataSet(te_x, te_y)
+    
+    return Datasets(train=train, test=test)
+
