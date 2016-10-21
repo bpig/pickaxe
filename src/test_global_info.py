97c0bee97b27153bf52d1629038e69b8aa4977b5
shuai.li <286287737@qq.com>
2016-10-21 11:04:49 +0800

    gb

---
+++ src/test_global_info.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# __author__ = "shuai.li(286287737@qq.com)"
+# __date__ = "2016/10/21"
+from unittest import TestCase
+
+class TestGlobalInfo(TestCase):
+    def test_process(self):
+        self.fail()
