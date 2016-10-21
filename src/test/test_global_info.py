99f9096f1b1eb3dece38f3c576c9d2ee9b454874
shuai.li <286287737@qq.com>
2016-10-21 14:15:02 +0800

    gb

--- src/test/test_global_info.py
+++ src/test/test_global_info.py
@@ -2,7 +2,8 @@
 # __author__ = "shuai.li(286287737@qq.com)"
 # __date__ = "2016/10/21"
 from unittest import TestCase
+from src.global_info import process
 
 class TestGlobalInfo(TestCase):
     def test_process(self):
-        self.fail()
+        process("small.ft", "small.gb")
