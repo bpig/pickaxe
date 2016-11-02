63504922983eeb51af8e5815f321fc5b6e7a7655
austin <286287737@qq.com>
2016-11-02 20:01:35 +0800

    add filter

--- src/gain_by_p.py
+++ src/gain_by_p.py
@@ -2,117 +2,93 @@
 from collections import Counter
 
 from common import *
+from src.data_loader import getFt, getAns
 
-def loadFile(fin):
-    kv = {}
-    for l in open(fin):
-        l = l.strip()
-        if not l:
-            continue
-        pos = l.find(",")
-        key = l[:pos]
-        items = l[pos + 1:].split(",")
-        items = map(lambda x: x.split("_"), items)
-        kv[key] = items
-    return kv
-
-def gain(predict, stock, numStock, period, start):
-    st2016 = set(map(str.strip, open("data/2016.st")))
-    ds = sorted(predict.keys())
+def gain(predict, stock, numStock, ds):
     totalMoney = [0.5, 0.5]
     updateMoney = [0.5, 0.5]
-    if start not in ds:
-        print ds
-        print "start date is too late"
-        return -1, totalMoney
-    ss = ds.index(start)
-    ds = ds[ss:ss+period]
+    
     for i, d in enumerate(ds):
-        stCt = 0
         buy = ["", ""]
         increase = [0, 0]
         count = [0, 0]
-        ii = i % 2;
-        nobuy = 0
-        nosell = 0
+        ii = i % 2
+        high = 0
+        low = 0
         stop = 0
         for rec in predict[d]:
-            if len(predict[d]) < 1000:
-                break
+            # if len(predict[d]) < 1000:
+            #     break
             if count[ii] == numStock:
                 break
-            key = rec[0]
-            if key not in stock.keys():
-                continue
-            if key in st2016:
-                stCt += 1
-                continue
-            items = stock[key]
-            if d not in items[0]:
-                continue
-            index = items[0].index(d) - 1
-            if index < 1:
+            key = rec.code
+            assert key in stock.keys(), "ft keys error, %s not exist" % key
+            
+            info = stock[key]
+            assert d in info.ds, "%s not exist" % d
+            
+            index = info.ds.index(d) - 1  # tomorrow
+            if index == 0:
                 continue
             # stop or +10 high
-            if items[15][index] == '1' or items[16][index] == '1':
-                if items[15][index] == '1':
-                    stop += 1
-                else:
-                    nobuy += 1
+            if info.status[index] == '1':
+                stop += 1
                 continue
-            inPrice = float(items[5][index])
-            nextDayIndex = index - 1
+            if info.s_status[index] == '1':
+                high += 1
+                continue
+            inPrice = float(info.s[index])
+            index -= 1  # the day after tomorrow
             # stop or -10 low
-            while nextDayIndex >= 0 and \
-                  (items[15][nextDayIndex] == '1' or items[18][nextDayIndex] == '2'):
-                nextDayIndex -= 1
-                if items[15][nextDayIndex] == '1':
-                    stop += 1
-                else:
-                    nosell += 1
-            if nextDayIndex < 0:
-                continue
-            outPrice = float(items[8][nextDayIndex])
-            #buy[ii] += str(key) + "_" + rec[1] + "_" + rec[2] + ","
-            buy[ii] += str(key) + "_" + rec[2] + ","
+            while index >= 0 and (info.status[index] == '1' or info.e_status[index] == '2'):
+                index -= 1
+            
+            if info.status[index + 1] == '1':
+                stop += 1
+            else:
+                low += 1
+            
+            if index < 0:
+                print "warning, %s low everyday" % key
+                index = 0
+            outPrice = float(info.e[index])
+            buy[ii] += key + "_" + rec.tgt + ","
             increase[ii] += outPrice / inPrice
             count[ii] += 1
-        # print d, len(predict[d]), totalMoney[ii], count[ii], increase[ii]
+        
         if count[ii] != 0:
             updateMoney[ii] = (increase[ii] / count[ii] - 0.0015) * totalMoney[ii]
-        # print d, "start: " + str(totalMoney[ii]), "end: " + str(updateMoney[ii]),
-        #  "buy " + str(count[ii]) + " stocks: " +buy[ii]
         
-            lt = buy[ii].split(",")[-2]
         bg = totalMoney[ii]
         ed = updateMoney[ii]
         lack = ""
         if count[ii] < numStock:
-            # print "no enough number of stocks to buy in"
             lack = "lack(%d)" % count[ii]
-
+        
         totalMoney[ii] = updateMoney[ii]
         total = sum(totalMoney)
-        print d, "%.8f" % total, "%.8f" % bg, "->", "%.8f" % ed, "%.8f" % (ed / bg), nobuy, nosell, stCt, stop, len(predict[d]), lack #, buy[ii]
+        print d, "%.8f" % total, "%.8f" % bg, "->", "%.8f" % ed, \
+            "%.8f" % (ed / bg), high, low, stop, len(predict[d]), lack  # , buy[ii]
     
-    return i, totalMoney
+    return i, sum(totalMoney)
 
-def process(predictFile, stockFile, numStock, period, start):
-    stock = loadFile(stockFile)
-    predict = {}
-    for k, v in loadFile(predictFile).items():
-        v = sorted(v, key=lambda x: float(x[1]), reverse=True)
-        predict[k] = v
-    i, money = gain(predict, stock, numStock, period, start)
+def process(predictFile, numStock, period, start):
+    stock = getFt("data/2010/2016.ft")
+    predict = dict(getAns(predictFile))
+    
+    ds = predict.keys()
+    idx = ds.index(start)
+    ds = ds[idx:idx + period]
+    
+    i, money = gain(predict, stock, numStock, ds)
     print "after " + str(i + 1) + " days:"
-    print "final: " + str(sum(money))
+    print "final: " + `money`
 
 if __name__ == "__main__":
     pfile = "ans/" + sys.argv[1]
-
-    stockFile = "data/2010/2016.ft"
+    
     try:
-        numStock = 50 
+        numStock = 50
         numStock = int(sys.argv[2])
     except:
         pass
@@ -123,12 +99,11 @@ if __name__ == "__main__":
         pass
     try:
         start = "20160104"
-        #start = "20161020"
+        # start = "20161020"
         start = sys.argv[4]
     except:
         pass
-
-    process(pfile, stockFile, numStock, period, start)
-
+    
+    process(pfile, numStock, period, start)
+    
     # process("../data/2016.ans", "../data/2016.ft", 100, 180, "20160104")
-
