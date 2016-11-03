1696d225880b5102579b173e318fba4c89e42b24
lee <286287737@qq.com>
2016-11-03 20:15:35 +0800

    update......

--- run.sh
+++ run.sh
@@ -8,9 +8,10 @@ python src/tflearn_predict.py v1503 today
 python src/tflearn_predict.py v1502 today
 python src/tflearn_predict.py v1504 today
 python src/tflearn_predict.py v1505 today
+python src/tflearn_predict.py v1506 today
 
 python src/combine_predict.py today
-python src/filter_by_rule.py today
+python src/filter_by_rule.py today.filter
 
 python src/daily.py today.filter 100 > daily/$2.csv
 
