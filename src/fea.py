4dcb5fde7dd83e9358039fbf81b307b9ee603c68
lee <286287737@qq.com>
2016-11-04 17:30:01 +0800

    update.....

--- src/fea.py
+++ src/fea.py
@@ -2,46 +2,14 @@
 from itertools import chain
 
 from common import *
-from data_loader import getFt, Aux
-
-# key           600227.SH
-# values
-# 0  dt         20160309_20160308_20160307_20160306_20160305_20160304,
-# 1  rate       3.0_3.0_3.0_-9.9_-9.9_3.0,
-# 2  volumn     3000_3000_3000_3000_3000_3000,
-# 3  amount     3000_3000_3000_3000_3000_3000,
-# 4  pe         10.0_10.0_10.0_10.0_10.0_10.0,
-# 5  start      10.1_10.1_8.3_8.19_9.1_10.1,
-# 6  high       10.3_10.3_8.3_8.19_9.1_10.1,
-# 7  low        10.0_10.0_8.3_8.19_9.1_10.1,
-# 8  end        10.2_10.2_8.3_8.19_9.1_10.1,
-# 9  turnover   2.0_2.0_4.0_3.0_3.0_2.0,
-# 10 shares     3000_3000_3000_3000_3000_3000,
-# 11 s-rate     1.01_1.01_0.83_0.819_0.91_1.01,
-# 12 h-rate     1.03_1.03_0.83_0.819_0.91_1.01,
-# 13 l-rate     1.0_1.0_0.83_0.819_0.91_1.01,
-# 14 e-rate     1.02_1.02_0.83_0.819_0.91_1.01,
-# 15 status     0_0_0_0_0_0,
-# 16 s-status   0_0_2_2_0_0,
-# 17 wav-status 0_0_2_2_0_0,
-# 18 e-status   0_0_2_2_0_0,
-# 19 target     -1.0_-1.0_1.0099009901_1.22891566265_1.01343101343_0.9
-
-
-# gb:
-# amount, shares * e, status-0, status-1, s-status-0, s-status-1, s-status-2,
-#  0            1        2         3         4            5            6
-# wav-status-0, wav-status-1, wav-status-2, wav-status-3, e-status-0, e-status-1, e-status-2,
-#   7                 8             9          10           11          12          13
-# amount / (shares * e), rate
-#       14                 15
+from data_loader import getFt, Aux, Ft
 
 def dump(st, fout, ds, predict=False):
     ct = 0
-    for items in st.items():
-        if ds not in items.ds[0]:
+    for key, info in st.items():
+        if ds not in info.ds:
             continue
-        content = genOne(items, ds, predict)
+        content = genOne(key, info, ds, predict)
         if content:
             fout.write(content)
             ct += 1
@@ -53,7 +21,7 @@ def daySpan(d1, d2):
     return (v2 - v1).days
 
 def genBasic(vals):
-    vals = np.array(vals)
+    vals = np.asarray(vals, dtype=np.float32)
     res = [sum(vals), np.mean(vals), np.std(vals), max(vals), min(vals)]
     # if len(vals) > 7:
     #     res += [stats.skewtest(vals)]
@@ -62,11 +30,14 @@ def genBasic(vals):
     return res
 
 def genStatus(status):
-    ct0 = Counter(map(int, status[0]))
-    ct1 = Counter(map(int, status[1]))
-    ct2 = Counter(map(int, status[2]))
-    ct3 = Counter(map(int, status[3]))
-    return [ct0[0], ct0[1], ct1[0], ct1[1], ct1[2], ct2[0], ct2[1], ct2[2], ct2[3], ct3[0], ct3[1], ct3[2]]
+    ct0 = Counter(status[0])
+    ct1 = Counter(status[1])
+    ct2 = Counter(status[2])
+    ct3 = Counter(status[3])
+    return [ct0['0'], ct0['1'], 
+            ct1['0'], ct1['1'], ct1['2'], 
+            ct2['0'], ct2['1'], ct2['2'], ct2['3'], 
+            ct3['0'], ct3['1'], ct3['2']]
 
 def oneHotStatus(status, sstatus, wavstatus, estatus):
     arr1 = [0] * 2
@@ -82,10 +53,8 @@ def oneHotStatus(status, sstatus, wavstatus, estatus):
     arr4[int(estatus)] = 1
     return arr1 + arr2 + arr3 + arr4
 
-def genOne(kv, ds, predict=False):
+def genOne(key, info, ds, predict=False):
     feas = []
-    key, info = kv
-    
     idx = info.ds.index(ds)
     
     if not predict:
@@ -94,16 +63,15 @@ def genOne(kv, ds, predict=False):
         # stock is stoped
         if info.status[idx - 1] == 1 or info.status[idx - 2] == 1:
             return ""
-    else:
-        if idx != 0:
-            print "index %s of %s must 0" % (ds, key)
-            return ""
+    elif idx != 0:
+        print "index %s of %s must 0" % (ds, key)
+        return ""
     
     windows = [2, 3, 5, 7, 15]  # , 30, 60]
     # windows = [2, 3, 5, 7, 10, 15, 20]  # , 30, 60]
     # windows = [2, 3, 5, 7, 10]  # , 30, 60]
     max_win = windows[-1]
-    info = map(lambda x: x[idx:idx + max_win], info)
+    info = Ft(*map(lambda x: x[idx:idx + max_win], info))
     
     if len(info.ds) != max_win:
         # print "%s_%s, %d" % (key, ds, len(values[0]))
@@ -112,14 +80,15 @@ def genOne(kv, ds, predict=False):
     # today day fea
     for d in range(max_win):
         feas += [info[_][d] for _ in [1, 2, 3, 9, 10, 11, 12, 13, 14]]
-        feas += oneHotStatus(info.status[d], info.s_status[d], info.wav_status[d], info.e_status[d])
+        feas += oneHotStatus(info.status[d], info.s_status[d], 
+                             info.wav_status[d], info.e_status[d])
     
     # win fea
     for window in windows:
         fea = []
-        items = map(lambda x: x[:window], info)
+        items = Ft(*map(lambda x: x[:window], info))
         span = daySpan(items.ds[-1], items.ds[0])
-        gain = items.e[0] / items.s[-1]
+        gain = float(items.e[0]) / float(items.s[-1])
         for i in [1, 2, 3, 9, 10, 11, 12, 13, 14]:
             fea += genBasic(items[i])
         fea += genStatus(items[15:19])
