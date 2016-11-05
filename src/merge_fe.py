4199dcbbdcd6d154a50c0c36d048d2b940d5af84
lee <286287737@qq.com>
2016-11-05 16:17:26 +0800

    update.....

--- src/merge_fe.py
+++ src/merge_fe.py
@@ -3,22 +3,24 @@ __author__ = "shuai.li(286287737@qq.com)"
 __date__ = "10/31/16"
 
 from common import *
-from mlp_feeder import read_data_sets
-
-def getFeaKeys(filename):
-    for l in open(filename):
-        pass
-
-def mergeSmallCsv(uniq):
-    smallCsvDir = "data/predict/cache"
-    for d in os.listdir(smallCsvDir):
-        if len(d) > 12 or not d.endswith(".fe"):
-            continue
-        smallCsvDir + "/" + d
-        print d, len(kv), len(uniq)
 
 if __name__ == '__main__':
-    with open("conf/fea.yaml") as fin:
-        cfg = yaml.load(fin)[sys.argv[1]]
+    fin = sys.argv[1]
+    fout = open(sys.argv[2], "w")
+
+    files = os.listdir(fin)
+    files = sorted(files)
+    print "total %d files" % len(files)
+
+    for c, l in enumerate(files):
+        if "dumper.list" in l:
+            continue
+        tgt = fin + "/" + l
+        fout.write(l)
+        fout.write(":")
+        fout.write(open(tgt).read())
+        fout.write("\n")
+        if c % 10000 == 0:
+            print time.ctime(), c
     
-    fin = "data/" + cfg["test"]
+
