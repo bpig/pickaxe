b40f81ab0c38734063d0d257a0712de04a8c89c4
lee <286287737@qq.com>
2016-11-08 21:16:27 +0800

    update....

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 set -x
 
-python src/gen_predict_fea.py  $1 #20161102
+# python src/gen_predict_fea.py  $1 #20161102
 
-python src/format.py f1
+# python src/format.py f1
 
 python src/predict.py v1501 today
 python src/predict.py v1503 today
@@ -12,11 +12,11 @@ python src/predict.py v1502 today
 python src/predict.py v1504 today
 python src/predict.py v1506 today
 
-python src/combine_ans.py today
+#python src/combine_ans.py today
 
 python src/combine_ans.py top
 
-python src/daily.py today > daily/$2.csv
+#python src/daily.py today > daily/$2.csv
 
 python src/top.py top
 
