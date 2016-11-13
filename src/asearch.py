fee1c436290eb30613615bc20a369466056bb5ec
austin <286287737@qq.com>
2016-11-13 17:14:14 +0800

    add fea

--- src/asearch.py
+++ src/asearch.py
@@ -13,4 +13,19 @@ if __name__ == '__main__':
             continue
         key = r[0]
         name = key + "_" + r[1]
-        
+        s3 = float(r[2])
+        s50 = float(r[3])
+        t3[key] += [(name, s3)]
+        t50[key] += [(name, s50)]
+    
+    for k in t3.keys():
+        t3[k] = sorted(t3[k], key=lambda x: -x[1])
+        t50[k] = sorted(t50[k], key=lambda x: -x[1])
+
+    print "top 3 select"
+    for k in t3.keys():
+        print k, t3[k][:5]
+
+    print "top 50 select"
+    for k in t50.keys():
+        print k, t50[k][:5]
