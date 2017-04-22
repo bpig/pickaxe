dcbad075bc284cd394ef227728067f6364d0add9
shuai.li <286287737@qq.com>
2017-04-22 21:51:45 +0800

    format..................

--- train/train_and_test_Regression.py
+++ train/train_and_test_Regression.py
@@ -16,7 +16,7 @@ from keras.layers import *
 from keras.models import model_from_json
 from keras.utils import plot_model
 from keras.utils import np_utils
-from keras.callbacks import LearningRateScheduler, EarlyStopping  
+from keras.callbacks import LearningRateScheduler, EarlyStopping
 import keras.backend as K
 import tensorflow as tf
 
@@ -25,6 +25,7 @@ import helpfuncs.date as date
 import helpfuncs.finance as finance
 from helpfuncs.ChooseChannel import ChooseChannel
 from models import *
+
 
 def init_log(save_path, name, phase):
     current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -47,11 +48,11 @@ def init_log(save_path, name, phase):
     logger.addHandler(fh)
     logger.addHandler(ch)
 
+
 def load_data(states, data_length, interval):
     for state in states:
         current_dir = os.path.dirname(os.path.realpath(__file__))
-        DATA_DIR = "data/%s_pydata_L%d_T%d/" % (
-            state, data_length, interval)
+        DATA_DIR = "data/%s_pydata_L%d_T%d/" % (state, data_length, interval)
         DATA_DIR = os.path.join(current_dir, DATA_DIR)
         print 'loading %s data' % state
         globals()[state + '_data'] = np.load(DATA_DIR + state + 'Data.npy')
@@ -60,24 +61,21 @@ def load_data(states, data_length, interval):
         globals()[state + '_labels'] = np.zeros_like(globals()[state + '_delta'], dtype=np.int32)
         if state == 'test':
             print 'loading test date'
-            globals()[state + '_saledate'] = np.load(DATA_DIR +
-                                                     state + 'SaleDate.npy')
-            globals()[state + '_buydate'] = np.load(DATA_DIR +
-                                                    state + 'BuyDate.npy')
+            globals()[state + '_saledate'] = np.load(DATA_DIR + state + 'SaleDate.npy')
+            globals()[state + '_buydate'] = np.load(DATA_DIR + state + 'BuyDate.npy')
             print 'loading test info'
             globals()[state + '_info'] = np.load(DATA_DIR + state + 'Info.npy')
             globals()[state + '_float_profit'] = np.load(DATA_DIR + state + 'FloatProfit.npy')
-        if state == 'train':    
-            globals()[state + '_turnover'] = np.load(DATA_DIR +
-                                                     state + 'Turnover.npy')
-            globals()[state + '_vr'] = np.load(DATA_DIR +
-                                                     state + 'VR.npy')
+        if state == 'train':
+            globals()[state + '_turnover'] = np.load(DATA_DIR + state + 'Turnover.npy')
+            globals()[state + '_vr'] = np.load(DATA_DIR + state + 'VR.npy')
+
 
 def load_index(filedir):
     csvfile = open(filedir)
     csvfile.readline()
     reader = csv.reader(csvfile)
-    index_dict_first= dict()
+    index_dict_first = dict()
     index_dict_last = dict()
     for line in reader:
         date_str = date.datetostr(date.strtodatetime(line[0], '%Y-%m-%d'))
@@ -85,7 +83,9 @@ def load_index(filedir):
         index_dict_last[date_str] = float(line[3])
     return index_dict_first, index_dict_last
 
-def train(save_path, interval, data_length, nb_channel, delta_scale=20., lr=0.01, gamma=0.1, n_epochs=[5, 6, 6], weight_decay=0.001, batch_size=128):
+
+def train(save_path, interval, data_length, nb_channel, delta_scale=20., lr=0.01, gamma=0.1, n_epochs=[5, 6, 6],
+          weight_decay=0.001, batch_size=128):
     current_dir = os.path.dirname(os.path.realpath(__file__))
     states = ['train', 'test']
 
@@ -135,7 +135,7 @@ def train(save_path, interval, data_length, nb_channel, delta_scale=20., lr=0.01
     sgd = SGD(lr=lr, momentum=0.9)
     scheduler = LearningRateScheduler(lr_scheduler)
     model.compile(optimizer=sgd, loss='mse')
-    history = model.fit(train_data, train_delta*delta_scale, 
+    history = model.fit(train_data, train_delta * delta_scale,
                         batch_size=batch_size, epochs=sum(n_epochs), callbacks=[scheduler])
     logger.info(str(dict(zip(history.epoch, history.history['loss']))))
 
@@ -151,6 +151,7 @@ def train(save_path, interval, data_length, nb_channel, delta_scale=20., lr=0.01
 
     print 'training finished'
     return
+
 
 def process_predictions(pred, N, test_start_day, test_end_day, name):
     logger = logging.getLogger(name)
@@ -182,7 +183,7 @@ def process_predictions(pred, N, test_start_day, test_end_day, name):
             date_num[current_date] = 1
         if test_info[i][0] > 0.0:
             p = signal
-            if maxNp.has_key(current_date) == False:
+            if not maxNp.has_key(current_date):
                 maxNp[current_date] = [p]
                 maxNi[current_date] = [i]
             else:
@@ -193,7 +194,7 @@ def process_predictions(pred, N, test_start_day, test_end_day, name):
                         maxNi[current_date].insert(k, i)
                         insert_flag = True
                         break
-                if insert_flag == False:
+                if not insert_flag:
                     maxNp[current_date].append(p)
                     maxNi[current_date].append(i)
                 if len(maxNp[current_date]) > N:
@@ -203,7 +204,9 @@ def process_predictions(pred, N, test_start_day, test_end_day, name):
     logger.info('pos/total: %f of total: %d' % (pos_total / processed_total, total))
     return maxNp, maxNi, sumP, sumRealProfit, date_num
 
-def compute_profits(maxNp, maxNi, sumP, sumRealProfit, date_num, cost, interval, test_start_day, test_end_day, threshold, delta_scale, index_dict_first, index_dict_last, name, weight=False):
+
+def compute_profits(maxNp, maxNi, sumP, sumRealProfit, date_num, cost, interval, test_start_day, test_end_day,
+                    threshold, delta_scale, index_dict_first, index_dict_last, name, weight=False):
     logger = logging.getLogger(name)
     logger.info('--------------------------------------')
     # compute every day profit
@@ -211,12 +214,12 @@ def compute_profits(maxNp, maxNi, sumP, sumRealProfit, date_num, cost, interval,
     current_day = date.strtodatetime(test_start_day, format)
     oneday = datetime.timedelta(days=1)
     total_day = date.datediff(date.datetostr(current_day), test_end_day) + 1
-    net_arr = np.ones((total_day))
-    index_arr = np.ones((total_day))
-    hedged_arr = np.ones((total_day))
-    meanpredchosen_arr = np.zeros((total_day))
-    realprof_arr = np.zeros((total_day))
-    marketprof_arr = np.zeros((total_day))
+    net_arr = np.ones(total_day)
+    index_arr = np.ones(total_day)
+    hedged_arr = np.ones(total_day)
+    meanpredchosen_arr = np.zeros(total_day)
+    realprof_arr = np.zeros(total_day)
+    marketprof_arr = np.zeros(total_day)
     index_init = 0.
     current_profit = 1.0
     day = 1
@@ -245,15 +248,14 @@ def compute_profits(maxNp, maxNi, sumP, sumRealProfit, date_num, cost, interval,
         else:
             market_profit = 0.
         if maxNi.has_key(day_str):
-            mean_pred_chosen = np.mean(maxNp[day_str])/delta_scale
+            mean_pred_chosen = np.mean(maxNp[day_str]) / delta_scale
         else:
             mean_pred_chosen = 0.
         logger.info('mean prediction: ' + str(mean_pred / delta_scale))
         logger.info('mean prediction chosen: ' + str(mean_pred_chosen))
         if index_arr[day] != index_arr[day - 1] and sumP.has_key(day_str):
             trade_day += 1
-        # get position
-        position = 1
+
         if mean_pred_chosen <= threshold[0]:
             position = 0
         elif mean_pred_chosen > threshold[1]:
@@ -274,31 +276,32 @@ def compute_profits(maxNp, maxNi, sumP, sumRealProfit, date_num, cost, interval,
         # buy
         real_prof = 0.0
         today_limitdown = 0.0
-        if maxNi.has_key(day_str) and position > 0.:
+        if day_str in maxNi and position > 0.:
             today_amount = 0.0
             rank = 0
             for (i, p) in zip(maxNi[day_str], maxNp[day_str]):
                 rank += 1
                 info = test_info[i]
                 amount = position * \
-                    (cash[it] if day > (interval + 1) else 1. / (interval + 1))
+                         (cash[it] if day > (interval + 1) else 1. / (interval + 1))
                 if weight:
                     l = len(maxNi[day_str])
-                    amount = amount * (1.0-(rank-1)/float(l)) / (float(l)-((float(l)-1)/2.0))
+                    amount = amount * (1.0 - (rank - 1) / float(l)) / (float(l) - ((float(l) - 1) / 2.0))
                 else:
                     amount /= N
-                logger.info('buying code: %06d' % info[2] + ', pred: %.5f' % (p / delta_scale) + ', amount: %.5f' % amount 
-                            + ', last 3 day mean amount:%.4f'%(info[0]/10) + ', limit down:'+ str(info[1]))
+                logger.info(
+                    'buying code: %06d' % info[2] + ', pred: %.5f' % (p / delta_scale) + ', amount: %.5f' % amount
+                    + ', last 3 day mean amount:%.4f' % (info[0] / 10) + ', limit down:' + str(info[1]))
                 holding_stocks.append((i, amount, day_str, it, trade_day))
                 today_amount += amount
-                mean_amount += info[0]/10
+                mean_amount += info[0] / 10
                 # statistics
                 valid_all += 1
                 d = test_delta[i]
                 real_prof += d
-                if d-cost > 0:
+                if d - cost > 0:
                     acc += 1
-                if d-cost < 0:
+                if d - cost < 0:
                     neg += 1
                 if info[1]:
                     limit_down += 1
@@ -307,8 +310,8 @@ def compute_profits(maxNp, maxNi, sumP, sumRealProfit, date_num, cost, interval,
             real_prof /= len(maxNi[day_str])
             it += 1
             it = it % (interval + 1)
-            logger.info('pred and real: %f %f'%(mean_pred_chosen, real_prof))
-	    logger.info('today limit down: %d'%today_limitdown)
+            logger.info('pred and real: %f %f' % (mean_pred_chosen, real_prof))
+            logger.info('today limit down: %d' % today_limitdown)
         # sell
         logger.info('selling: ')
         today_profit = 0.0
@@ -323,8 +326,10 @@ def compute_profits(maxNp, maxNi, sumP, sumRealProfit, date_num, cost, interval,
                 cash[this_it] += amt * (1 + delta)
                 remove_buffer.append((i, amt, buy_date, this_it, buy_day))
         for (i, amt, buy_date, this_it, buy_day) in remove_buffer:
-            holding_stocks.remove(((i, amt, buy_date, this_it, buy_day)))
-        float_profit = np.sum([test_float_profit[i][min(trade_day-buy_day, interval-1)]*amt for (i, amt, buy_date, this_it, buy_day) in holding_stocks])
+            holding_stocks.remove((i, amt, buy_date, this_it, buy_day))
+        float_profit = np.sum(
+            [test_float_profit[i][min(trade_day - buy_day, interval - 1)] * amt for (i, amt, buy_date, this_it, buy_day)
+             in holding_stocks])
         meanpredchosen_arr[day] = mean_pred_chosen
         realprof_arr[day] = real_prof
         marketprof_arr[day] = market_profit
@@ -336,7 +341,7 @@ def compute_profits(maxNp, maxNi, sumP, sumRealProfit, date_num, cost, interval,
         # compute hedged profit and print all profit data
         hedged_arr[day] = 1 + (net_arr[day] - index_arr[day])
         logger.info('today profit: ' + str(today_profit))
-        logger.info('float profit: %f'%(float_profit))
+        logger.info('float profit: %f' % float_profit)
         logger.info('today index profit: ' +
                     str(index_arr[day] / index_arr[day - 1] - 1.))
         logger.info('net: ' + str(net))
@@ -347,19 +352,19 @@ def compute_profits(maxNp, maxNi, sumP, sumRealProfit, date_num, cost, interval,
         logger.info('--------------------------------------')
     logger.info('Pos: ' + str(acc / valid_all) +
                 ' Neg: ' + str(neg / valid_all))
-    logger.info('Correlation: ' + str(np.corrcoef(meanpredchosen_arr,realprof_arr)[0][1]))
+    logger.info('Correlation: ' + str(np.corrcoef(meanpredchosen_arr, realprof_arr)[0][1]))
     logger.info('Sharpe Ratio: raw=%f, hedged=%f' %
                 (finance.SharpeRatio(net_arr), finance.SharpeRatio(hedged_arr)))
     logger.info('Max Retreat: raw=%f, hedged=%f' %
                 (finance.MaxRetreat(net_arr), finance.MaxRetreat(hedged_arr)))
-    logger.info('3 day mean amount %f'%(mean_amount/valid_all))
+    logger.info('3 day mean amount %f' % (mean_amount / valid_all))
     logger.info(' limit down: ' + str(limit_down))
     logger.info('valid_all: ' + str(valid_all))
-    #plt.plot(np.arange(0, day), realprof_arr-marketprof_arr, 'r')
-    diff = realprof_arr-marketprof_arr
+    # plt.plot(np.arange(0, day), realprof_arr-marketprof_arr, 'r')
+    diff = realprof_arr - marketprof_arr
     diff = diff[np.nonzero(diff)] - cost
-    WinRate = float(len(diff[diff>0]))/float(np.count_nonzero(diff))
-    logger.info( 'Daily win rate: %f'%WinRate )
+    WinRate = float(len(diff[diff > 0])) / float(np.count_nonzero(diff))
+    logger.info('Daily win rate: %f' % WinRate)
     '''plt.bar(np.arange(0, len(diff)), diff, color='r')
     plt.xlabel('days')
     plt.ylabel('diff')
@@ -367,8 +372,9 @@ def compute_profits(maxNp, maxNi, sumP, sumRealProfit, date_num, cost, interval,
     plt.show()'''
     return net_arr, index_arr, hedged_arr
 
+
 def test(save_path, data_length, interval, test_start_day, test_end_day, name,
-         threshold=[0.0, 0.0], cost=0.0015,  N=20, delta_scale=20., recompute=False):
+         threshold=[0.0, 0.0], cost=0.0015, N=20, delta_scale=20., recompute=False):
     start_time = time.time()
     INDEX_DIR = '/home/yingyang/DeepQuant/data/index/zh500.csv'
     logger = logging.getLogger(name)
@@ -379,16 +385,16 @@ def test(save_path, data_length, interval, test_start_day, test_end_day, name,
     logger.info('loading index')
     index_dict_first, index_dict_last = load_index(INDEX_DIR)
 
-    if recompute == False and os.path.isfile(pred_path):
+    if not recompute and os.path.isfile(pred_path):
         pred = np.load(pred_path)
     else:
         # load model
         model_path = os.path.join(current_dir, save_path + "/model.json")
-        #model = makeModel(data_length=15, nb_channel=39, weight_decay=0.001)
-        #f = open(model_path, 'w')
-        #model_json = model.to_json()
-        #f.write(model_json)
-        #f.close
+        # model = makeModel(data_length=15, nb_channel=39, weight_decay=0.001)
+        # f = open(model_path, 'w')
+        # model_json = model.to_json()
+        # f.write(model_json)
+        # f.close
         f = open(model_path, 'r')
         model_json = f.read()
         f.close
@@ -404,22 +410,22 @@ def test(save_path, data_length, interval, test_start_day, test_end_day, name,
 
     # process the predictions into dictionaries
     logger.info('processing...')
-    print np.max(pred/delta_scale)
+    print np.max(pred / delta_scale)
     maxNp, maxNi, sumP, sumRealProfit, date_num = process_predictions(
         pred, N, test_start_day, test_end_day, name)
     # compute every day profit
     profit_arr, index_arr, hedged_arr = \
         compute_profits(maxNp, maxNi, sumP, sumRealProfit, date_num, cost, interval,
                         test_start_day, test_end_day, threshold, delta_scale, index_dict_first, index_dict_last, name)
-    #np.save(os.path.join(current_dir, save_path + 'profit.npy'), profit_arr)
-    #np.save(os.path.join(current_dir, save_path + 'hs300.npy'), index_arr)
+    # np.save(os.path.join(current_dir, save_path + 'profit.npy'), profit_arr)
+    # np.save(os.path.join(current_dir, save_path + 'hs300.npy'), index_arr)
 
     maxNcode = {}
     for buydate in maxNi:
         for i in maxNi[buydate]:
             saledate = test_saledate[i]
             delta = test_delta[i]
-            code = '%06d'%test_info[i][2]
+            code = '%06d' % test_info[i][2]
             if maxNcode.has_key(buydate):
                 maxNcode[buydate].append(code)
             else:
@@ -442,19 +448,20 @@ def test(save_path, data_length, interval, test_start_day, test_end_day, name,
     logger.info('bye')
     return
 
+
 if __name__ == '__main__':
     with open(os.path.expanduser("~/.keras/keras.json"), "w") as fh:
         fh.write('{ "image_dim_ordering": "tf", "epsilon": 1e-07, "floatx": "float32", "backend": "tensorflow" }')
     phase = sys.argv[1]
-    phase_train = phase=='train'
-    test_recompute = phase_train or phase=='retest'
+    phase_train = phase == 'train'
+    test_recompute = phase_train or phase == 'retest'
     data_length = int(sys.argv[2])
     nb_channel = 41
     interval = 5
     cost = 0.0015
     N = 20
     lr = 0.01
-    n_epoch = [8,6,6,4]
+    n_epoch = [8, 6, 6, 4]
     weight_decay = 0.001
     batch_size = 2048
     delta_scale = 10.0
@@ -462,7 +469,7 @@ if __name__ == '__main__':
     model_name = sys.argv[3]
     names = sys.argv[4:]
     makeModel = globals()[model_name]
-    #makeModel = globals()[name.split('_')[0]]
+    # makeModel = globals()[name.split('_')[0]]
     test_start_day = '20160701'
     test_end_day = '20170101'
 
@@ -472,14 +479,14 @@ if __name__ == '__main__':
 
     for name in names:
         current_dir = os.path.dirname(os.path.realpath(__file__))
-        save_path = 'L%d/' % data_length + '%s/'%model_name + '%s' % name + '_T%d/' % interval
-        if os.path.exists(os.path.join(current_dir, 'L%d/' % data_length)) == False:
+        save_path = 'L%d/' % data_length + '%s/' % model_name + '%s' % name + '_T%d/' % interval
+        if not os.path.exists(os.path.join(current_dir, 'L%d/' % data_length)):
             os.mkdir(os.path.join(current_dir, 'L%d/' % data_length))
-	if os.path.exists(os.path.join(current_dir, 'L%d/' % data_length + '%s/'%model_name)) == False:
-            os.mkdir(os.path.join(current_dir, 'L%d/' % data_length + '%s/'%model_name))
-        if os.path.exists(os.path.join(current_dir, save_path)) == False:
+        if not os.path.exists(os.path.join(current_dir, 'L%d/' % data_length + '%s/' % model_name)):
+            os.mkdir(os.path.join(current_dir, 'L%d/' % data_length + '%s/' % model_name))
+        if not os.path.exists(os.path.join(current_dir, save_path)):
             os.mkdir(os.path.join(current_dir, save_path))
-        #train and test
+        # train and test
         init_log(save_path + test_start_day + '_' + test_end_day +
                  '_%.3f_%.3f_%d_' % (threshold[0], threshold[1], N), name, 'test')
         if phase_train:
