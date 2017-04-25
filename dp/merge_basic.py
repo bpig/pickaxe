7657d292e9eff14b440b79382ca2724f013a4ba5
lee <286287737@qq.com>
2017-04-25 18:12:12 +0800

    update.........

--- dp/merge_basic.py
+++ dp/merge_basic.py
@@ -1,15 +1,17 @@
 import os
 
-print os.path.abspath(os.curdir)
-from dp.common import *
+from common import *
 
 if __name__ == '__main__':
     st_list = sorted(os.listdir(BASIC_DATA))
     df = None
-    for st in st_list[:2]:
+    for st in st_list[:2] + ["300359.csv"]:
+        print st
         stock = pd.read_csv(os.path.join(BASIC_DATA, st), parse_dates=[0])
-        if not df:
+        stock['code'] = st[:-4]
+        if df is None:
             df = stock.copy()
         else:
-            df.append(st, axis=1)
-    df.to_csv("merge.cc", ignore_index=False)
+            df = df.append(stock, ignore_index=True)
+    df.fillna(0, inplace=True)
+    df.to_csv("merge.cc", index=False, header=False)
